0a0eb09f392e829cedc92c479b58adc93c542269
Alehxalves <65253818+Alehxalves@users.noreply.github.com>
2020-09-18 11:58:30 -0300

    Add files via upload

---
+++ Projeto_final.c
@@ -0,0 +1,17 @@
+/* Projeto Final De FUP-2020 
+    Sistema de Quiz
+
+Deve Contem:
+
+Menu de Usuário.
+Cadastro de Usuário.
+Perguntas
+Resposta do usuário
+Informação de erro ou acerto da pergunta.
+Pontuação
+
+Menu Admin
+Cadastro de Perguntas juntamente com as resposta.
+Excluir pergunta.
+
+*/
